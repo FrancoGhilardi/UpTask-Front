065592a6af3c848c9f2e13bba93f52b3d57747b7
franco <franco,ghilardi1@gmail.com>
2024-08-15 22:30:49 -0300

    Se comienza con la creacion de rutas y formularios

--- src/views/DashBoardView.tsx
+++ src/views/DashBoardView.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const DashBoardView: React.FC = () => {
-  return <div>asdhbasjhkd</div>;
+  return (
+    <Fragment>
+      <h1 className="text-5xl font-black">Mis Proyectos</h1>
+      <p className=" text-2xl font-light text-gray-500 mt-5">
+        Maneja y administra tus proyectos
+      </p>
+      <nav className="my-5">
+        <Link
+          to="/projects/create"
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3  text-white text-xl font-bold cursor-pointer transition-colors"
+        >
+          Nuevo Proyecto
+        </Link>
+      </nav>
+    </Fragment>
+  );
 };
 
 export default DashBoardView;
